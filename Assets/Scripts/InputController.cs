3516784730fd17477728c0a307af0b5d5e0848e5
ygjin07 <ygjin00@ajou.ac.kr>
2024-08-06 17:46:09 +0900

    Add Time

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -9,7 +9,11 @@ public class InputController : MonoBehaviour
 
     List<int> input_set = new List<int>();
     int score = 0;
+    [SerializeField]
     int life = 4;
+    [SerializeField]
+    float limit_time = 3.5f;
+    float time;
 
     //아ㅠ에서부터 순서대로 S, F, J, L, Space, Space
     NPCType[] key_set = (NPCType[])System.Enum.GetValues(typeof(NPCType));
@@ -17,6 +21,7 @@ public class InputController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        time = limit_time;
         ShuffleArray(key_set);
         Debug.Log("key_set : " + string.Join(", ", key_set));
     }
@@ -24,6 +29,12 @@ public class InputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time -= Time.deltaTime;
+        if(time < 0) 
+        {
+            
+        }
+
         if(Input.GetKeyDown(KeyCode.S)) 
         {
             KeyInput(0);
@@ -63,10 +74,17 @@ public class InputController : MonoBehaviour
 
             Debug.Log("Score : " + score + ", Life : " + life);
 
-            npc_controller.RemoveLine();
-            npc_controller.AddLine();
+            NextLine();
+
             input_set.Clear();
         }
+    }
+
+    void NextLine()
+    {
+        time = limit_time;
+        npc_controller.RemoveLine();
+        npc_controller.AddLine();
     }
 
     bool CompareLine()
@@ -111,10 +129,16 @@ public class InputController : MonoBehaviour
     void InputFail()
     {
         life--;
+        limit_time -= 0.1f;
+        if(life < 0)
+        {
+            Debug.Log("GAMEOVER");
+        }
     }
 
     void InputSuccess()
     {
         score++;
+        limit_time -= 0.1f;
     }
 }
